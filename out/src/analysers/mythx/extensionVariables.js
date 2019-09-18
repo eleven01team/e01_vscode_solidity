f986868054e8b8ac9e95ed0b0b6203537332f36c
vinod reddy <vinod@eleven01.io>
2019-09-18 09:38:11 +0530

    Removed Nodemodules

--- out/src/analysers/mythx/extensionVariables.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ext;
-(function (ext) {
-})(ext = exports.ext || (exports.ext = {}));
-//# sourceMappingURL=extensionVariables.js.map
